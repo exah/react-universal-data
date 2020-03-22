c704b0a949878a112c3b15ba5dc7d8d005ca503e
Ivan Grishin <r@exah.me>
2020-03-22 23:11:36 +0000

    🧹 Remove old docs

--- src/context.ts
+++ src/context.ts
@@ -1,28 +1,6 @@
 import { createContext } from 'react'
 import { defaultStore } from './store'
 
-/**
- * Provides `store` created with {@link createStore} to {@link useFetchData} hook using [React Context](http://reactjs.org/docs/context.html).
- *
- * #### Props
- *
- * - `value` **DataStoreType**
- *
- * @name DataProvider
- *
- * @example
- *
- * import React from 'react'
- * import { createStore, DataProvider } from 'react-universal-data'
- * import App from './app'
- *
- * const appElement = (
- *   <DataProvider value={createStore()}>
- *     <App />
- *   </DataProvider>
- * )
- */
-
 export const DataContext = createContext(defaultStore)
 
 export const DataProvider = DataContext.Provider
