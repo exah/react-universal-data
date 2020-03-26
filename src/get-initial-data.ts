4c166f8e8bed1ab9dbb950cc8f54dc0d82a5a897
Ivan Grishin <r@exah.me>
2020-03-26 00:37:44 +0000

    🚑 Fix element type

--- src/get-initial-data.ts
+++ src/get-initial-data.ts
@@ -2,7 +2,7 @@ import prepass from 'react-ssr-prepass'
 import { RawStore } from './types'
 import { defaultStore } from './store'
 
-function getInitialData(element: React.ElementType, store = defaultStore) {
+function getInitialData(element: JSX.Element, store = defaultStore) {
   store.clear()
 
   return prepass(element).then<RawStore>(() => Array.from(store))
